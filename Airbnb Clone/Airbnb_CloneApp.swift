e5675defd2411cb0cd83928470dcfe6404d43c62
ajs7777 <abhisaha955@gmail.com>
2024-12-24 20:44:51 +0530

    Initial Commit

---
+++ Airbnb Clone/Airbnb_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Airbnb_CloneApp.swift
+//  Airbnb Clone
+//
+//  Created by Abhijit Saha on 24/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Airbnb_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

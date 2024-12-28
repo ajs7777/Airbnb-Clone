4f80484ab30e568288bd9ef3be51ee30223a5d10
ajs7777 <abhisaha955@gmail.com>
2024-12-28 21:25:43 +0530

    create a fetch request through a view model

--- Airbnb Clone/Core/Explore/ViewModel/ExploreViewModel.swift
+++ Airbnb Clone/Core/Explore/ViewModel/ExploreViewModel.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+class ExploreViewModel: ObservableObject {
+    @Published var listings = [Listing]()
+    private let service : ExploreService
+    
+    init(service: ExploreService) {
+        self.service = service
+    }
+    
+    func fetchListings() async {
+        do {
+            self.listings = try await service.fetchListings()
+        } catch  {
+            print("DEBUG: Error fetching listings: \(error.localizedDescription)")
+        }
+    }
+}
